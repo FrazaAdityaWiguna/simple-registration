ca74dc3facd184c09f7e2adbdc4c433cf7b5d1cb
FrazaAdityaWiguna <adityafraja12@gmail.com>
2023-09-12 19:43:08 +0700

    fix: fixing package react-lottie

--- src/Components/CanNotAccess/CanNotAccess.tsx
+++ src/Components/CanNotAccess/CanNotAccess.tsx
@@ -2,20 +2,11 @@
 
 import React from "react";
 import { Button, Stack, Typography } from "@mui/material";
-import lottieWelcome from "@/assets/lottie/not-auth.json";
-import Lottie from "react-lottie";
+import lottieNotAuth from "@/assets/lottie/not-auth.json";
+import Lottie from "lottie-react";
 import Link from "next/link";
 
 const CanNotAccess = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: lottieWelcome,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
   return (
     <Stack
       alignItems="center"
@@ -23,7 +14,15 @@ const CanNotAccess = () => {
       sx={{ width: "100%", height: "100vh" }}
       gap={2}
     >
-      <Lottie options={defaultOptions} height={400} width={400} />
+      <Lottie
+        animationData={lottieNotAuth}
+        autoplay={true}
+        loop={true}
+        style={{
+          height: 400,
+          width: 400,
+        }}
+      />
       <Typography sx={{ fontWeight: "600", fontSize: "20px" }}>
         Upss... You can not access this page
       </Typography>
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -3,31 +3,30 @@
 import { Button, Stack, Typography } from "@mui/material";
 import Link from "next/link";
 import React from "react";
-import Lottie from "react-lottie";
+import Lottie from "lottie-react";
 import lottieWelcome from "@/assets/lottie/welcome.json";
 
 const Home = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: lottieWelcome,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
   return (
     <Stack
       alignItems="center"
       justifyContent="center"
       sx={{ width: "100%", height: "100vh" }}
     >
-      <Lottie options={defaultOptions} height={400} width={400} />
+      <Lottie
+        animationData={lottieWelcome}
+        autoplay={true}
+        loop={true}
+        style={{
+          height: 400,
+          width: 400,
+        }}
+      />
       <Typography align="center" sx={{ maxWidth: "500px" }}>
         This project is a simple user registration and management system,
-        allowing users to register, view, and delete user data. It utilizes the
+        allowing users to register, view, and delete user data. It utilizes the{" "}
         <Link href="https://reqres.in/" target="_blank">
-          reqres.in{" "}
+          reqres.in
         </Link>{" "}
         API as a mock API for testing and development purposes.
       </Typography>
